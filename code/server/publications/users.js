66539b0b12b6f990d7e7096830edfd28addf341d
Ryan Glover <me@ryanglover.net>
2015-10-08 09:00:58 -0500

    add admin role check to users publication

--- code/server/publications/users.js
+++ code/server/publications/users.js
@@ -1,6 +1,12 @@
 Meteor.publish( 'users', function() {
-  return [
-    Meteor.users.find( {}, { fields: { "emails.address": 1, "roles": 1 } } ),
-    Invitations.find( {}, { fields: { "email": 1, "role": 1, "date": 1 } } )
-  ];
+  let isAdmin = Roles.userIsInRole( this.userId, 'admin' );
+
+  if ( isAdmin ) {
+    return [
+      Meteor.users.find( {}, { fields: { "emails.address": 1, "roles": 1 } } ),
+      Invitations.find( {}, { fields: { "email": 1, "role": 1, "date": 1 } } )
+    ];
+  } else {
+    return null;
+  }
 });
